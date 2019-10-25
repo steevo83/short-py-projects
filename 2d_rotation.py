6b9d1f157c55e6e605a61bb18efe532b0f2241df
Steven Ormosi <sormosi@alpinebiz.com>
2019-10-25 16:05:44 -0400

    2d_rotation.py added
    First step in working out a way to make a changing block of rooms.
    As of now this is a 2d object that rotates one way. My attempt to
    rotate it back the other way have been unsuccessful thus far.

---
+++ 2d_rotation.py
@@ -0,0 +1,119 @@
+
+# Python3 program to rotate a matrix by 90 degrees 
+N = 4
+  
+# An Inplace function to rotate  
+# N x N matrix by 90 degrees in 
+# anti-clockwise direction 
+def rotateMatrix(mat, dir="left"): 
+      
+    # Consider all squares one by one 
+    print(f'max range for x: int({N} divided by 2) minus 1 = {int(N/2)-1}')
+    for x in range(0, int(N/2)): 
+        print(f'x: {x}')     
+        # Consider elements in group    
+        # of 4 in current square 
+        print(f'max range for y: ({N} minus {x} minus 1) minus one = {(N-x-1)-1}')
+        for y in range(x, N-x-1):
+            print(f'y: {y}')  
+            # store current cell in temp variable  
+            temp = mat[x][y]
+            if dir == "left":  
+                # move values from right to top 
+                mat[x][y] = mat[y][N-1-x] 
+    
+                # move values from bottom to right 
+                mat[y][N-1-x] = mat[N-1-x][N-1-y] 
+    
+                # move values from left to bottom 
+                mat[N-1-x][N-1-y] = mat[N-1-y][x] 
+    
+                # assign temp to left 
+                mat[N-1-y][x] = temp
+            elif dir == "right":  
+                # temp = mat[x][y]             
+                # move values from right to *bottom* 
+                mat[x][y] = mat[N-1-y][x]
+    
+                # move values from bottom to *left* 
+                mat[y][N-1-x] = mat[N-1-y][x]
+    
+                # move values from left to *top*
+                mat[N-1-x][N-1-y] = mat[y][N-1-x] 
+    
+                # assign temp to *right* 
+                mat[N-1-x][N-1-y] = temp
+  
+  
+# Function to pr the matrix 
+def displayMatrix( mat ): 
+      
+    for i in range(0, N): 
+          
+        for j in range(0, N): 
+              
+            print (mat[i][j], end = ' ') 
+        print ("") 
+      
+      
+  
+  
+# Driver Code 
+mat = [[0 for x in range(N)] for y in range(N)] 
+  
+
+
+# Test case 1 
+mat = [ [1, 2, 3, 4 ], 
+        [5, 6, 7, 8 ], 
+        [9, 10, 11, 12 ], 
+        [13, 14, 15, 16 ] ] 
+          
+rotateMatrix(mat) 
+  
+# Print rotated matrix 
+displayMatrix(mat) 
+print()
+
+# Test case 2 
+N=3
+
+mat = [ [1, 2, 3 ], 
+        [4, 5, 6 ], 
+        [7, 8, 9 ] ] 
+  
+rotateMatrix(mat)
+
+#Steve Test case 1
+mat = [ ["tl","tm", "tr"], 
+        ["ml","mm", "mr"],
+        ["bl","bm","br"] ]
+
+rotateMatrix(mat)
+  
+# Print rotated matrix 
+displayMatrix(mat) 
+print()
+
+print("Rotate right")
+rotateMatrix(mat, "right")
+displayMatrix(mat) 
+print()
+rotateMatrix(mat, "test")
+displayMatrix(mat) 
+print()
+
+# Test case 3 
+N=2
+mat = [ [1, 2 ], 
+        [4, 5 ] ] 
+          
+
+  
+rotateMatrix(mat) 
+  
+# Print rotated matrix 
+displayMatrix(mat) 
+  
+  
+# This code is contributed by saloni1297 
